0312e67cb05d7576adc6e7d96d808fa5262c3292
Sumit Chavan <sdchavan8070@gmail.com>
2025-01-25 17:35:49 +0530

    server changed

--- src/GlobalStrings/globalStrings.jsx
+++ src/GlobalStrings/globalStrings.jsx
@@ -1,12 +1,15 @@
 export const RENDER_SERVER_URL =
   "https://meadhikari-server.onrender.com/api/v1/admin";
 
+export const RENDER_SERVER_URL_6795 =
+  "https://meadhikari-server-6795.onrender.com/api/v1/admin";
+
 export const LOCAL_SERVER_URL = "http://192.168.43.78:8080/api/v1/admin";
 // export const LOCAL_SERVER_URL = "http://localhost:8080/api/v1/admin";
 
 export const VERCEL_SERVER = "https://meadhikari-sever.vercel.app/api/v1/auth";
 
-export const SERVER_URL = RENDER_SERVER_URL;
+export const SERVER_URL = RENDER_SERVER_URL_6795;
 
 {
   /* <table border="1">
