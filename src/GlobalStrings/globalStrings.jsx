6ba8635c36a0f043ff78bc72ee091fcf5f7a833b
Sumit Chavan <sdchavan8070@gmail.com>
2025-03-10 10:55:12 +0530

    Changed render server

--- src/GlobalStrings/globalStrings.jsx
+++ src/GlobalStrings/globalStrings.jsx
@@ -10,12 +10,15 @@ export const RENDER_SERVER_URL_SERVER1 =
 export const RENDER_SERVER_URL_SERVER2 =
   "https://server2-bfcq.onrender.com/api/v1/admin";
 
+export const RENDER_SERVER_URL_SERVER3 =
+  "https://server4-7qmk.onrender.com/api/v1/admin";
+
 export const LOCAL_SERVER_URL = "http://192.168.43.78:8080/api/v1/admin";
 // export const LOCAL_SERVER_URL = "http://localhost:8080/api/v1/admin";
 
 export const VERCEL_SERVER = "https://meadhikari-sever.vercel.app/api/v1/auth";
 
-export const SERVER_URL = RENDER_SERVER_URL_SERVER2;
+export const SERVER_URL = RENDER_SERVER_URL_SERVER3;
 
 {
   /* <table border="1">
